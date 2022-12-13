232accff39f9ea3fd0dc48821c4bae59e1f0ac48
ViJerDev <shizajob@gmail.com>
2022-12-13 20:25:34 +0300

    Reroll

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FirstController extends Controller
 {
-    public function index(){
-        return view('welcome');
+    public function index()
+    {
+        $latestProducts = Product::query()
+            ->where('status', true)
+            ->limit(10)
+            ->latest()
+            ->get();
+
+//        $categories = Category::all()->keyBy('id');
+
+        return view('welcome', compact('latestProducts'));
     }
 
 }
